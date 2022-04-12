ba4b64896385275730e0ed8ac0283b550d38d2d9
Christian Kyle Valdez <christiankyle.valdez@g.batstate-u.edu.ph>
2022-04-12 12:24:43 +0800

    directories

--- accountManagementAdmin.php
+++ accountManagementAdmin.php
@@ -2,13 +2,14 @@
 <html lang="en">
 <?php include "views/includes/head.php"; ?>
 <?php include "controllers/emptySessionController.php"; ?>
+<?php include "controllers/informationController.php"; ?>
 
 <body>
-    <?php include "views/headerAdmin.php"; ?>
+    <?php include "views/dashboard/header.php"; ?>
 
     <?php include "views/includes/sidebar.php"; ?>
 
-    <?php include "views/accountManagementAdmin.php"; ?>
+    <?php include "views/account/accountManagementAdmin.php"; ?>
 
 </body>
 
