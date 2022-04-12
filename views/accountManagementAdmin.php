db723bcc05f85dd7efb37ecd414aaa91e70a984d
valjuniel123 <valjuniel123@gmail.com>
2022-04-12 12:15:37 +0800

    Add modal to Account Management

--- views/accountManagementAdmin.php
+++ views/accountManagementAdmin.php
@@ -19,7 +19,7 @@
             <div class="card-body">
               <div class="d-flex justify-content-between">
                 <h5 class="card-title">Users</h5>
-                <button type="button" class="btn btn-dark btn-sm">Pending Accounts</button>
+                <button type="button" class="btn btn-dark btn-sm" data-bs-toggle="modal" data-bs-target="#accountPendings">Pending Accounts</button>
               </div>
               <p>People that registered their account to the system.</p>
 
@@ -46,5 +46,35 @@
         </div>
       </div>
     </section>
+
+    <!-- Create a modal with a table -->
+    <div class="modal fade" id="accountPendings" tabindex="-1" role="dialog" aria-labelledby="accountPendings" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="accountPendings">Pending Accounts</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <table class="table table-hover dt-responsive nowrap text-center" id="accountPendingTable">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">ID</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Campus</th>
+                  <th scope="col">Department</th>
+                  <th scope="col">Email</th>
+                  <th scope="col">Role</th>
+                  <th scope="col">Actions</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
 
   </main><!-- End #main -->
