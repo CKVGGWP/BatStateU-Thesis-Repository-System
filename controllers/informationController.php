53b0de95fc0370ae3c9df2199bb7063e02eb1a7b
Christian Kyle Valdez <christiankyle.valdez@g.batstate-u.edu.ph>
2022-04-13 08:08:18 +0800

    fix

--- controllers/informationController.php
+++ controllers/informationController.php
@@ -9,19 +9,21 @@ $id = isset($_SESSION['srCode']) ? $_SESSION['srCode'] : '';
 
 $campuses = $info->getCampuses();
 
-$user = $info->getUserBySession($id);
+if ($id != '') {
+    $user = $info->getUserBySession($id);
 
-$name = $user['firstName'] . ' ' . $user['lastName'];
-$firstName = $user['firstName'];
-$middleName = $user['middleName'];
-$lastName = $user['lastName'];
-$role = ($user['role'] == '1') ? 'Admin' : 'User';
-$title = ($user['role'] == '1') ? 'Librarian' : 'Student';
-$srCode = $user['srCode'];
-$email = $user['email'];
-$campus = $user['campusName'];
-$department = $user['departmentName'];
-$deptID = $user['deptID'];
-$campID = $user['campID'];
+    $name = $user['firstName'] . ' ' . $user['lastName'];
+    $firstName = $user['firstName'];
+    $middleName = $user['middleName'];
+    $lastName = $user['lastName'];
+    $role = ($user['role'] == '1') ? 'Admin' : 'User';
+    $title = ($user['role'] == '1') ? 'Librarian' : 'Student';
+    $srCode = $user['srCode'];
+    $email = $user['email'];
+    $campus = $user['campusName'];
+    $department = $user['departmentName'];
+    $deptID = $user['deptID'];
+    $campID = $user['campID'];
 
-$campusNotIncluded = $info->getCampuses($campID);
+    $campusNotIncluded = $info->getCampuses($campID);
+}
