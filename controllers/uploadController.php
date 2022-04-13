a1bbafb99717b96bfb863e557d5b670ccbc94960
Christian Kyle Valdez <christiankyle.valdez@g.batstate-u.edu.ph>
2022-04-13 10:35:01 +0800

    validation

--- controllers/uploadController.php
+++ controllers/uploadController.php
@@ -7,9 +7,5 @@ $upload = new Upload();
 if (isset($_POST['uploadAdmin'])) {
     print_r($_POST);
     print_r($_FILES);
-    echo $upload->uploadFiles($_POST, $_FILES);
+    // echo $upload->uploadFiles($_POST, $_FILES);
 }
-
-
-
-
