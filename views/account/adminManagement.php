2adc8bc542aee01b5729156a640f04e3f6d4a23d
Christian Kyle Valdez <christiankyle.valdez@g.batstate-u.edu.ph>
2022-04-18 15:23:08 +0800

    fix

--- views/account/adminManagement.php
+++ views/account/adminManagement.php
@@ -47,7 +47,7 @@
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="editAccount">Edit Admin Account</h5>
+                    <h5 class="modal-title" id="editAccount">Add Admin Account</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form id="createAdmin" method="post">
