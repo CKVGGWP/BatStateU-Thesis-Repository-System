f9e4c0a220fd3726ad67d9d6d55f189c44a5fce5
Christian Kyle Valdez <christiankyle.valdez@g.batstate-u.edu.ph>
2022-04-19 18:37:18 +0800

    dash

--- dashboard.php
+++ dashboard.php
@@ -16,19 +16,6 @@
 
         <?php if ($_GET['title'] == "Dashboard") : ?>
 
-            <?= print_r($_SESSION); ?>
-
-            <!-- Convert time() to readable format -->
-
-            <?php
-
-            $difference = time() - $_SESSION['time'];
-
-            echo $difference;
-
-
-            ?>
-
             <?php if ($_SESSION['role'] == '1') : ?>
 
                 <?php include "views/dashboard/dashboardAdmin.php"; ?>
